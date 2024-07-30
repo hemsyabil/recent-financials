a85168378a14d062d4c73ab4eef557bffe506d52
Hem Raj Regmi <sangamsyabil@gmail.com>
2024-07-30 19:26:48 +0000

    using session

--- app.py
+++ app.py
@@ -4,14 +4,19 @@ import humanize
 import pandas as pd
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_session import Session
 
 finance_app = Flask(__name__)
 finance_app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 finance_app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 finance_app.config['SECRET_KEY'] = 'your_secret_key'
+finance_app.config['SESSION_TYPE'] = 'filesystem'
 
 db = SQLAlchemy(finance_app)
-bcrypt = Bcrypt()
+bcrypt = Bcrypt(finance_app)
+
+# Initialize the Flask-Session extension
+Session(finance_app)
 
 
 class User(db.Model):
@@ -67,38 +72,52 @@ def login():
     return render_template('login.html')
 
 
+@finance_app.route('/logout')
+def logout():
+    session.pop('email', None)
+    return redirect('/login')
+
+
 @finance_app.route('/')
 def dashboard():
-    total_owed = humanize.intcomma(int(total_i_owe_all))
+    if 'email' in session:
+        user = User.query.filter_by(email=session['email']).first()
+        total_owed = humanize.intcomma(int(total_i_owe_all))
 
-    return render_template('dashboard.html',
-                           title="Financial Dashboard",
-                           name="Financial Dashboard",
-                           DATA_DICT=DATA_JSON,
-                           filtered_data=filtered_data,
-                           total_owed=total_owed)
+        return render_template('dashboard.html',
+                               title="Financial Dashboard",
+                               name="Financial Dashboard",
+                               user=user,
+                               DATA_DICT=DATA_JSON,
+                               filtered_data=filtered_data,
+                               total_owed=total_owed)
+
+    return redirect('/login')
 
 
 @finance_app.route('/<name>')
 def personal_dashboard(name):
-    sheet_name = name_mapping.get(name)
+    if 'email' in session:
+        sheet_name = name_mapping.get(name)
 
-    if not sheet_name:
-        return jsonify({"error": "Record not found"}), 404
+        if not sheet_name:
+            return jsonify({"error": "Record not found"}), 404
 
-    record = DATA_JSON.get(sheet_name, [])
-    total_owed = filtered_data[sheet_name]["Total I Owe"]
+        record = DATA_JSON.get(sheet_name, [])
+        total_owed = filtered_data[sheet_name]["Total I Owe"]
 
-    df = pd.DataFrame(record)
-    table_html = df.to_html(classes='table table-striped table-hover',
-                            index=False)
+        df = pd.DataFrame(record)
+        table_html = df.to_html(classes='table table-striped table-hover',
+                                index=False)
 
-    return render_template('personal-dashboard.html',
-                           title="Recent Financials - Dashboard",
-                           filtered_data=filtered_data,
-                           name=sheet_name,
-                           total_owed=total_owed,
-                           table=table_html)
+        return render_template('personal-dashboard.html',
+                               title="Recent Financials - Dashboard",
+                               filtered_data=filtered_data,
+                               name=sheet_name,
+                               total_owed=total_owed,
+                               table=table_html)
+
+    return redirect('/login')
 
 
 if __name__ == "__main__":
